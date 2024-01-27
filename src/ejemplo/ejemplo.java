856382e1c384c52c63da96dc7d9dca32fd9489ac
irvin <irving.gutierrez@uip.edu.pa>
2024-01-27 15:15:40 -0500

    holamundo

---
+++ src/ejemplo/ejemplo.java
@@ -0,0 +1,49 @@
+package ejemplo;
+
+public class ejemplo {
+
+    public static void main (String[] args) {
+        /*
+        impresion de un saludo
+        en pantalla. nada complejo
+         */
+        System.out.println("que xopa!");
+
+        // tipos de datos enteros
+
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 9223372036854775807L;
+
+        // tipos de datoa decimales
+        float floatVar = 3.14f;
+        double doubleVar = 3.1416;
+
+        // tipos de datos caracteres
+        char charVar = 'A';
+
+        // tipos de cadenas
+        String stringVar = "Chacalaca";
+
+        // tipos de datos boleanos
+        boolean booleanVar = true;
+
+        float salario, bono, multa;
+
+        System.out.println("byte: " + byteVar);
+        System.out.println("short: " + shortVar);
+        System.out.println("int: " + intVar);
+        System.out.println("long: " + longVar);
+        System.out.println("float: " + floatVar);
+        System.out.println("double: " + doubleVar);
+        System.out.println("char: " + charVar);
+        System.out.println("boolean: " + booleanVar);
+        System.out.println("string: " + stringVar);
+
+
+
+    }
+
+
+}
